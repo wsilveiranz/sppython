c30e31b08410b6be9570d6c1b3e999bd18663bad
Wagner Silveira <wagner.silveira@theta.co.nz>
2018-01-12 11:46:20 +1300

    including pandas in the requirements.txt

--- hellopython/run.py
+++ hellopython/run.py
@@ -1,10 +1,11 @@
 import os
 import platform
 import json
+import pandas  as pd
 
 postreqdata = json.loads(open(os.environ['req']).read())
 
-message = "Hello world from {0} - Using Python '{1}'".format(postreqdata['name'],platform.python_version())
+message = "Hello world from {0} - Using Python '{1}'. Pandas version is {2}".format(postreqdata['name'],platform.python_version(), pd.__version__)
 print(message)
 
 response = open(os.environ['res'], 'w')
