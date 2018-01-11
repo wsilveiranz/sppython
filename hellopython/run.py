213fdf96d965a8f261b00a58c7d9db2f279b2ab6
Wagner Silveira <wagner.silveira@theta.co.nz>
2018-01-12 11:33:14 +1300

    fixing binding, removing requirements.txt

--- hellopython/run.py
+++ hellopython/run.py
@@ -1,4 +1,12 @@
 import os
 import platform
-message = "Using Python '{0}'".format(platform.python_version())
+import json
+
+postreqdata = json.loads(open(os.environ['req']).read())
+
+message = "Hello world from {0} - Using Python '{1}'".format(postreqdata['name'],platform.python_version())
 print(message)
+
+response = open(os.environ['res'], 'w')
+response.write(message)
+response.close()
