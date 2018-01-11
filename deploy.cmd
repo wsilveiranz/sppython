a006b4f7a41560d3d9bca40f994a8a05acf799c4
Wagner Silveira <wagner.silveira@theta.co.nz>
2018-01-11 15:59:35 +1300

    rolling back to site extensions (3.6.1)

--- deploy.cmd
+++ deploy.cmd
@@ -74,7 +74,7 @@ pushd "%DEPLOYMENT_TARGET%"
 IF NOT EXIST "D:\home\site\tools\python36*" (
 
   echo Creating Python 3.6.1 x64 virtual environment...
-  nuget.exe install -Source https://www.nuget.org/api/v2 -OutputDirectory D:\home\site\tools python -Version 3.6.3
+  nuget.exe install -Source https://www.siteextensions.net/api/v2/ -OutputDirectory D:\home\site\tools python361x64
   mv /d/home/site/tools/python.3*/tools/* /d/home/site/tools/
   IF !ERRORLEVEL! NEQ 0 goto error
 
