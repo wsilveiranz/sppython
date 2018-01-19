4951cc30d78f6d3b38ad059a83eb73e4d508abf6
Wagner Silveira <wagner.silveira@theta.co.nz>
2018-01-19 16:21:55 +1300

    creating first spacy script

---
+++ findsentences/run.py
@@ -0,0 +1,17 @@
+import spacy
+import json
+import os
+
+postreqdata = json.loads(open(os.environ['req']).read())
+
+#create the spaCy model
+nlp = spacy.load('en_core_web_lg')
+
+document = nlp(postrequest)
+
+for token in document:
+    print(token.text)
+
+response = open(os.environ['res'], 'w')
+response.write(len(doucment.tokens))
+response.close()
